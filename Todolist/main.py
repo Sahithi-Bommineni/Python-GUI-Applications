80645b756fb59f5a51d1c8675228bd1775efa2f5
sahithi-bommineni <bommineni.s@northeastern.edu>
2025-07-14 17:46:24 -0500

    added edit and complete feature to todolist

--- Todolist/main.py
+++ Todolist/main.py
@@ -7,8 +7,16 @@ sg.theme("Brown Blue")
 label = sg.Text("Enter task: ")
 task_input = sg.InputText(tooltip="Enter Task", key = 'task')
 add_button = sg.Button("Add")
+task_list = sg.Listbox(values=functions.read_file(),key='tasks',enable_events=True,size=[45,10])
+edit_button = sg.Button("Edit")
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 
-window = sg.Window("My Todo List",layout=[[label],[task_input,add_button]])
+layout=[[label],
+        [task_input,add_button],
+        [task_list,edit_button,complete_button,exit_button]]
+
+window = sg.Window("My Todo List",layout)
 while True:    
     event, values = window.read()
     match event:
@@ -17,6 +25,27 @@ while True:
             task = values['task'] + '\n'
             tasks.append(task)
             functions.write_file(tasks)
+            window['tasks'].update(values=tasks)
+            window['task'].update(value="")
+        case 'Edit':
+            task_to_edit=values['tasks'][0]
+            new_task = values['task']
+            tasks = functions.read_file()
+            index = tasks.index(task_to_edit)
+            tasks[index]=new_task
+            functions.write_file(tasks)
+            window['tasks'].update(values=tasks)
+        case 'tasks':
+            window['task'].update(value = values['tasks'][0])
+        case 'Complete':
+            task_complete = values['tasks'][0]
+            tasks=functions.read_file()
+            tasks.remove(task_complete)
+            functions.write_file(tasks)
+            window['tasks'].update(values=tasks)
+            window['task'].update(value='')
+        case 'Exit':
+            break
         case sg.WIN_CLOSED:
             break
 
